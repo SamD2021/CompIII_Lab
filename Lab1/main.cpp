0832b03126a1d47eebd0c41cb451ea25a4ec10cd
CamiSalinas24 <camilas2407@gmail.com>
2023-09-13 11:39:42 -0400

    Update main.cpp

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -2,13 +2,31 @@
 
 using namespace std;
 
+void user_input (double& annual_interest_rate, double& amount_to_receive, int& loan_period);
 double calculateLoanAmount(double initialFaceValue, double interestRate, int durationMonths, double& monthlyPayment);
 
 int main(int argc, char *argv[]) {
+    double annual_interest_rate, amount_to_receive;
+    int loan_period;
+
+    user_input(annual_interest_rate, amount_to_receive, loan_period);
 
     return 0;
 }
 
+void user_input (double& annual_interest_rate, double& amount_to_receive, int& loan_period)
+{
+    cout << "Please enter the annual interest rate as a percentage: ";
+    cin >> annual_interest_rate;
+
+    if (annual_interest_rate != 0) {
+        cout << "Please enter the amount you would like to receive: ";
+        cin >> amount_to_receive;
+        
+        cout << "Please enter loan period in months: ";
+        cin >> loan_period;
+    }
+}
 
 double calculateLoanAmount(double amountNeeded, double interestRate, int durationMonths, double& monthlyPayment){
     double durationYears;
