a5ca378efe132905c4eb3f2f010c101ff6a96769
Sam <samuelramos852@gmail.com>
2023-09-12 23:08:23 -0400

    Implemented calculateLoanAmount

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+double calculateLoanAmount(double initialFaceValue, double interestRate, int durationMonths, double& monthlyPayment);
+
 int main(int argc, char *argv[]) {
 
-
     return 0;
+}
+
+
+double calculateLoanAmount(double amountNeeded, double interestRate, int durationMonths, double& monthlyPayment){
+    double durationYears;
+    double totalInterest;
+    double faceValue;
+    double amountReceived;
+    interestRate = interestRate/100; // transition from percentage to decimal
+    durationYears = durationMonths/12.0;
+    totalInterest = (amountNeeded * (interestRate)) * durationYears;
+    amountReceived = amountNeeded - totalInterest;
+    faceValue = amountReceived/(1-(interestRate*durationYears));
+    monthlyPayment = faceValue/durationMonths;
+
+    return amountReceived;
 }
