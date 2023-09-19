6700aeeebe84372e20b3e42281fdb3436f219b06
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-09-19 15:36:04 -0400

    Created Driver function

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+
+void driver_code();
+
+int main(int argc, char* argv[]){
+    driver_code(); // executes all the functions
+    return 0;
+}
+
+void driver_code(){
+    // call all functions from here
+}
