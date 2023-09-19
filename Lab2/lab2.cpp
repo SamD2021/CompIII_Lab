ff5a3e6f81ac071376860aa1cf144b63fb1821ce
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-09-19 18:58:11 -0400

    merged calculation and output

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -2,9 +2,14 @@
 #include <cmath>
 
 using namespace std;
+// functions declaration below
 
 void driver_code();
 void displayConversion (int pounds, double ounces, int kilograms, double grams);
+void convert_pounds_and_oz_to_kilograms_and_grams(int pounds, double oz, int& kilograms, double& grams);
+
+// Constants
+const double KG = 0.45359237;
 
 int main(int argc, char* argv[]){
     driver_code(); // executes all the functions
@@ -13,7 +18,7 @@ int main(int argc, char* argv[]){
 
 void driver_code(){
     // call all functions from here
-
+    // convert_pounds_and_oz_to_kilograms_and_grams(pounds,ounces, kilograms, grams);
     // displayConversion(pounds, ounces, kilograms, grams);
 }
 
@@ -23,4 +28,13 @@ void displayConversion (int pounds, double ounces, int kilograms, double grams)
     cout << pounds << " pounds and " << ounces << " ounces converts to " << endl;
     cout.precision(8);
     cout << kilograms << " kilograms and " << fixed << grams << " grams " << endl;
+}
+
+void convert_pounds_and_oz_to_kilograms_and_grams(int pounds, double oz, int& kilograms, double& grams){
+    double totalPounds;
+    double totalWeight;
+    totalPounds = (oz/16.0) + pounds;
+    totalWeight = totalPounds * KG;
+    kilograms = totalWeight;
+    grams = (totalWeight - kilograms) * 1000;
 }
