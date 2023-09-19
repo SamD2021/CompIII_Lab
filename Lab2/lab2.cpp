104f931b7e64b6fb4721174a561da9f070d9019f
CamiSalinas24 <camilas2407@gmail.com>
2023-09-19 15:59:38 -0400

    Update lab2.cpp

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <cmath>
 
+using namespace std;
 
 void driver_code();
+void displayConversion (int pounds, double ounces, int kilograms, double grams);
 
 int main(int argc, char* argv[]){
     driver_code(); // executes all the functions
@@ -10,4 +13,12 @@ int main(int argc, char* argv[]){
 
 void driver_code(){
     // call all functions from here
+}
+
+
+void displayConversion (int pounds, double ounces, int kilograms, double grams)
+{
+    cout << pounds << " pounds and " << ounces << " ounces converts to " << endl;
+    cout.precision(8);
+    cout << kilograms << " kilograms and " << fixed << grams << " grams " << endl;
 }
