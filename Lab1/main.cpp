c193beefc8e6a4119f64b2608be1a4fa1d3ac84a
LukaM21 <malak_metias@student.uml.edu>
2023-09-19 15:45:06 -0400

    LOL

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+    int month;
+    double payment;
+    int  endvalue;
+
+    cout<< "please enter the loan period: ";
+    cin>> month;
+    
 
     return 0;
 }
