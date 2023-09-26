0b74d16e796331a35500ebcae6159d002a404645
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-09-26 16:12:41 -0400

    Mass class header and implementation

--- Lab3/Mass.h
+++ Lab3/Mass.h
@@ -8,15 +8,16 @@
 
 class Mass {
 public:
-    Mass():drams(0){}
     void setMassAvoirdupoisPounds(double mass);
     void setMassTroyPounds(double mass);
     void setMassMetricGrams(double mass);
-    double getMassAvoirdupoisPounds() const;
-    double getMassTroyPounds() const;
-    double getMassMetricGrams() const;
+    double getMassAvoirdupoisPounds(void);
+    double getMassTroyPounds(void);
+    double getMassMetricGrams(void);
 private:
-    double drams;
+    double avoirdupoisPounds;
+    double troyPounds;
+    double metricGrams;
 };
 
 
