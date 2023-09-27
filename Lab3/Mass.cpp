067f225702b48ab247a289a9c7d25bd1a4d5998f
CamiSalinas24 <camilas2407@gmail.com>
2023-09-27 13:03:53 -0400

    Minor fix

--- Lab3/Mass.cpp
+++ Lab3/Mass.cpp
@@ -4,40 +4,8 @@
 
 #include <iostream>
 #include "Mass.h"
-<<<<<<< HEAD
-double const GRAMS = 1.7718451953125;
 
-void Mass::setMassAvoirdupoisPounds(double mass) {
-    drams = mass*256;
-;
-}
-
-double Mass::getMassAvoirdupoisPounds() const  {
-    return drams/256;
-}
-
-void Mass::setMassTroyPounds(double mass)
-{
-    drams = mass*96;
-}
-
-double Mass::getMassTroyPounds() const
-{
-    return drams/96;
-}
-
-void Mass::setMassMetricGrams(double mass) {
-    drams = mass/GRAMS;
-}
-
-double Mass::getMassMetricGrams() const {
-    return drams * GRAMS;
-}
-
-
-=======
-
-using namespace std;
+using namespace std; 
 
 void Mass::setMassTroyPounds(double mass)
 {
