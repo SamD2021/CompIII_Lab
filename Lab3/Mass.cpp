d187ed6126fe6ed503f8961efc48576b634c63aa
CamiSalinas24 <camilas2407@gmail.com>
2023-09-27 13:04:10 -0400

    Minor fix 2

--- Lab3/Mass.cpp
+++ Lab3/Mass.cpp
@@ -11,7 +11,6 @@ void Mass::setMassTroyPounds(double mass)
 {
     if (mass >= 0) { // check if non-negative
         troyPounds = mass; // stores input mas value directly since it's already in Troy pounds
-        avoirdupoisPounds = mass/96.0; // converts input mass to avoirdupois (1 Troy = 1/96 Avoirdupois)
         metricGrams = mass * 373.2417; // converts input mass to grams (1 Troy = 373.24g)
     } else {
         cout << "Error: Mass cannot be negative." << endl; // if negative -> error message
