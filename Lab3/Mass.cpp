f306d417060d0d8f0ae0933e577534ef3b3f16d8
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-09-30 14:43:56 -0400

    added Set and get avoirdupois pounds

--- Lab3/Mass.cpp
+++ Lab3/Mass.cpp
@@ -3,33 +3,13 @@
 //
 
 #include "Mass.h"
-double const GRAMS = 1.7718451953125;
 
 void Mass::setMassAvoirdupoisPounds(double mass) {
-    drams = mass*256;
-;
+    double convertedMass;
+    convertedMass = mass/256;
+    avoirdupoisPounds = convertedMass;
 }
 
 double Mass::getMassAvoirdupoisPounds() const  {
-    return drams/256;
+    return avoirdupoisPounds;
 }
-
-void Mass::setMassTroyPounds(double mass)
-{
-    drams = mass*96;
-}
-
-double Mass::getMassTroyPounds() const
-{
-    return drams/96;
-}
-
-void Mass::setMassMetricGrams(double mass) {
-    drams = mass/GRAMS;
-}
-
-double Mass::getMassMetricGrams() const {
-    return drams * GRAMS;
-}
-
-
