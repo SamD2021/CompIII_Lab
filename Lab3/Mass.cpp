0126fb4600b0971b03e135b790f38b90ee17e3fc
LukaM21 <malak_metias@student.uml.edu>
2023-10-02 09:33:46 -0400

    changed the mass class

--- Lab3/Mass.cpp
+++ Lab3/Mass.cpp
@@ -5,21 +5,12 @@
 #include <iostream>
 #include "Mass.h"
 
-using namespace std; 
+void Mass::setMassMetricGrams(double mass){
 
-void Mass::setMassTroyPounds(double mass)
-{
-    if (mass >= 0) { // check if non-negative
-        troyPounds = mass; // stores input mas value directly since it's already in Troy pounds
-        avoirdupoisPounds = mass/96.0; // converts input mass to avoirdupois (1 Troy = 1/96 Avoirdupois)
-        metricGrams = mass * 373.2417; // converts input mass to grams (1 Troy = 373.24g)
-    } else {
-        cout << "Error: Mass cannot be negative." << endl; // if negative -> error message
-    }
+    metricGrams = mass * 1.7718451953125;
 }
 
-double Mass::getMassTroyPounds(void)
+double Mass::getMassMetricGrams(void) const
 {
-    return troyPounds;
+    return metricGrams;
 }
->>>>>>> ad7b400 (Set and get troyPounds functions implemented)
