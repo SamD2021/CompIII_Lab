441b83448065ed9e28d409b61f0e83e806ad3ad8
melissaing <144846295+melissaing@users.noreply.github.com>
2023-10-03 12:40:43 -0400

    input code
    added switch statement and while loops for validation

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <string>
 
-cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit:" <<;
+using namespace std;
 
-string chosenUnit;
+int main(){
+    int userInput;
+    string chosenUnit = "";
+    double units;
+    
+    do {
+        cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit: ";
+        cin >> userInput;
+        
+        switch (userInput){
+        case 0:
+            cout << "Thanks for using the mass conversion program!" << endl;
+            break;
+        case 1:
+            chosenUnit = "Avoirdupois pounds";
+            break;
+        case 2:
+            chosenUnit = "Troy pounds";
+            break;
+        case 3:
+            chosenUnit = "grams";
+            break;
+        default:
+            cout << "Not a valid number, please try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while ((userInput < 0) || (userInput > 3));
 
-switch (userInput){
-    case 0:
-        return 0;
-        break;
-    case 1:
-        chosenUnit = "Avoirdupois pounds";
-        break;
-    case 2:
-        chosenUnit = "Troy pounds";
-        break;
-    case 3:
-        chosenUnit = "grams";
-        break;
-    default:
-        while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
-        cout << "Not a valid number, please try again. " << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (chosenUnit != ""){
+        while ((cout << "Please enter a mass in " << chosenUnit << ": ") && (!(cin >> units) || units < 0.0)) {
+            cout << "That is not a number greater than or equal to 0. " << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
     }
+
+    return 0;
 }
-
-cout << "Please enter a mass in " << chosenUnit << ": "
-while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
-        cout << "That is not a number greater than or equal to 0. " << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-
-/*
-Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit: 1
-Please enter a mass in Avoirdupois pounds: 1
-*/
