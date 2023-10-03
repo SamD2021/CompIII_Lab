3f81c82e28d27115b965eeed9b80f5828c7c3ed4
Sam <samuelramos852@gmail.com>
2023-10-03 12:15:53 -0400

    Receives desired output and follows the instructions closely.

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -5,40 +5,49 @@
 using namespace std;
 int main(int argc, char* argv[]){
 
-    cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit:" << endl;
-    int userInput = 1;
+    int userInput = 0;
     double pounds;
     string chosenUnit;
     Mass myMass;
-    switch (userInput){
-        case 0:
-            return 0;
-            break;
-        case 1:
-            chosenUnit = "Avoirdupois pounds";
-            break;
-        case 2:
-            chosenUnit = "Troy pounds";
-            break;
-        case 3:
-            chosenUnit = "grams";
-            break;
-        default:
-            while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
-                cout << "Not a valid number, please try again. " << endl;
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-    }
-
-    cout << "Please enter a mass in " << chosenUnit << ": ";
-    while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
-        cout << "That is not a number greater than or equal to 0. " << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    myMass.setMassAvoirdupoisPounds(pounds);
-    cout << myMass.getMassAvoirdupoisPounds() << endl;
+    do{
+        cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit:" << endl;
+        cin >> userInput;
+        switch (userInput){
+            case 0:
+                return 0;
+                break;
+            case 1:
+                chosenUnit = "Avoirdupois pounds";
+            case 2:
+                chosenUnit = "Troy pounds";
+                break;
+            case 3:
+                chosenUnit = "grams";
+                break;
+            default:
+                while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
+                    cout << "Not a valid number, please try again. " << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+        }
+        cout << "Please enter a mass in " << chosenUnit << ": ";
+        while ((cout << "Please enter the number of pounds: ") && (!(cin >> pounds) || pounds < 0)) {
+            cout << "That is not a number greater than or equal to 0. " << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        if(chosenUnit == "Avoirdupois pounds"){
+            myMass.setMassAvoirdupoisPounds(pounds);
+        }else if(chosenUnit == "Troy pounds"){
+            myMass.setMassTroyPounds(pounds);
+        }else if (chosenUnit == "grams"){
+            myMass.setMassMetricGrams(pounds);
+        }
+        cout << myMass.getMassAvoirdupoisPounds() << endl;
+        cout << myMass.getMassTroyPounds() << endl;
+        cout << myMass.getMassMetricGrams() << endl;
+    }while(userInput);
 
     return 0;
 }
