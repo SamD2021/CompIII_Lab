31a4273188e132ff2bce4f5f6a32919b91c5a5ad
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-10-17 15:46:30 -0400

    added driver with class prototype

---
+++ Lab5/lab5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ComplexNumber{
+public:
+    ComplexNumber();
+
+private:
+    int real;
+    string imaginary;
+};
+
+
+int main(int argc, char* argv[]){
+    return 0;
+}
