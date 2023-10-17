fe9354a0e015bff3a5a6569dbc1c022b62761016
CamiSalinas24 <camilas2407@gmail.com>
2023-10-17 15:52:14 -0400

    Added accessor and mutator member functions.

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -14,4 +14,18 @@ public:
     // Value constructor
     ComplexNumber(double r, double i): realNum(r), imagNum(i) {}
 
+    // Accessor functions
+    double getRealNum() const { return realNum; }
+    double getImagNum() const { return imagNum; }
+
+    // Mutator functions
+    double setRealNum(double r) { realNum = r; }
+    double setImagNum(double i) { imagNum = i; }
 };
+
+int main (int argc, char* argv[])
+{
+    // Driver code
+    
+    return 0;
+}
