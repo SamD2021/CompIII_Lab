3a2156333985f11b9d7dbeaee2a6dca3e0ee8351
CamiSalinas24 <camilas2407@gmail.com>
2023-10-17 15:47:41 -0400

    Added default and value constructors

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+class ComplexNumber {
+private:
+    int realNum;
+    int imagNum;
+
+public:
+    // Default constructor
+    ComplexNumber(): realNum(0), imagNum(0) {}
+
+    // Value constructor
+    ComplexNumber(double r, double i): realNum(r), imagNum(i) {}
+
+};
