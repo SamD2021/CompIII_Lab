820fdb22d0ab7a16181c917fd8ea61cae4902f15
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-10-17 15:54:09 -0400

    full class declaration added

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -6,7 +6,16 @@ using namespace std;
 class ComplexNumber{
 public:
     ComplexNumber();
-
+    int getReal()const;
+    void setReal(int num);
+    void setImaginary(const string& num);
+    string getImaginary()const;
+    friend ostream& operator<<(ostream& out, const ComplexNumber& cn);
+    ComplexNumber& operator+(const ComplexNumber& rhs);
+    ComplexNumber& operator-(const ComplexNumber& rhs);
+    ComplexNumber& operator*(const ComplexNumber& rhs);
+    ComplexNumber& operator/(const ComplexNumber& rhs);
+    ComplexNumber& operator!(const ComplexNumber& rhs);
 private:
     int real;
     string imaginary;
