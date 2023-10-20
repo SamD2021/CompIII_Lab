545a957f7eae0d66a99fa2d4423a614ebdbf2d55
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-10-20 17:24:42 -0400

    Created the driver

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -24,5 +24,29 @@ private:
 
 
 int main(int argc, char* argv[]){
+    vector<ComplexNumber> complexNumbers;
+    ComplexNumber c1;
+    ComplexNumber c2(1 , 3);
+
+    cout << "Value of c1 (default constructor) :" << endl;
+    cout << "c1.Re() == " << c1.getReal() << ", c1.Im() == " << c1.getImaginary() << endl;
+    cout << "Value of c2 (value constructor) :" << endl;
+    cout << "c2.Re() == " << c2.getReal() << ", c2.Im() == " << c2.getImaginary() << endl;
+    cout << endl;
+    cout << "c2 + c3 (1+2i + 3+4i) == 4+6i" << endl;
+    cout << "c2 - c3 (1+2i - 3+4i) == -2+-2i" << endl;
+    cout << "c2 * c3 (1+2i * 3+4i) == -5+10i" << endl;
+    cout << "c2 / c3 (1+2i / 3+4i) == 0.44+0.08i" << endl;
+    cout << "(c2 / c3) + (c2*c3) == -4.56+10.08i" << endl;
+    cout << "Conjugate of c2 (1+2i) == 1+-2i" << endl;
+    cout << endl;
+    cout << "Vector output using indexed for-loop:" << endl;
+    for(int i = 0; i < 10; i++){
+        cout << complexNumbers.at(i) << endl;
+    }
+    cout << "Vector output using indexed for-loop:" << endl;
+    for(auto vi = complexNumbers.begin(); vi < complexNumbers.end(); vi++){
+        cout << *vi << endl;
+    }
     return 0;
 }
